e8bc845c82ae2a6cf21ccf121ef68b29a7e7413a
Henrik Sandklef <hesa@sandklef.com>
2019-06-11 23:52:19 +0200

    debug, errror to stderr

--- src/se/juneday/lifegame/util/Log.java
+++ src/se/juneday/lifegame/util/Log.java
@@ -6,62 +6,71 @@ import java.io.PrintStream;
 
 public class Log {
 
-    public enum LogLevel {
-      QUIET,
-      ERROR,
-      WARN,
-      INFO,
-      DEBUG,
-      VERBOSE;
+  public enum LogLevel {
+    QUIET,
+    ERROR,
+    WARN,
+    INFO,
+    DEBUG,
+    VERBOSE;
 
-    }
+  }
 
-    private static LogLevel logLevel = LogLevel.ERROR;
-    private static String includeFilter ;
-    private static String includeTag ;
-    private static PrintStream out = System.out;
+  private static LogLevel logLevel = LogLevel.ERROR;
+  private static String includeFilter ;
+  private static String includeTag ;
+  private static PrintStream out = System.out;
+  private static PrintStream err = System.err;
+ 
+  public static void logLevel(LogLevel logLevel) {
+    Log.logLevel = logLevel;
+  }
 
-    public static void logLevel(LogLevel logLevel) {
-        Log.logLevel = logLevel;
-    }
+  public static void includeFilter(String includeFilter) {
+    Log.includeFilter = includeFilter;
+  }
 
-    public static void includeFilter(String includeFilter) {
-        Log.includeFilter = includeFilter;
-    }
+  public static void includeTag(String includeTag) {
+    Log.includeTag = includeTag;
+  }
 
-    public static void includeTag(String includeTag) {
-        Log.includeTag = includeTag;
-    }
-
-    private static void printTagMessage(String tag, String message) {
-        if (includeTag!=null) {
-          if (!tag.contains(includeTag)) {
-                return;
-            }
-        }
-        if (includeFilter!=null) {
-            if (!message.contains(includeFilter)) {
-                return;
-            }
-        }
-        out.println(tag + ":" + message);
-    }
-
-    public static void d(String tag, String message) {
-      if (logLevel.compareTo(LogLevel.DEBUG) >= 0) {
-        printTagMessage(tag, message);
+  private static void printTagMessage(String tag, String message, PrintStream stream) {
+    if (includeTag!=null) {
+      if (!tag.contains(includeTag)) {
+        return;
       }
     }
-
-    public static void e(String tag, String message) {
-      if (logLevel.compareTo(LogLevel.ERROR) >= 0) {
-        printTagMessage(tag, message);
+    if (includeFilter!=null) {
+      if (!message.contains(includeFilter)) {
+        return;
       }
     }
+    stream.println(tag + ":" + message);
+  }
 
-    public static void i(String tag, String message) {
-        if (logLevel.compareTo(LogLevel.INFO) >= 0) {
-            printTagMessage(tag, message);
-        }
+  private static void printTagMessage(String tag, String message) {
+    printTagMessage(tag, message, out);
+  }
+
+  private static void printErrTagMessage(String tag, String message) {
+    printTagMessage(tag, message, err);
+  }
+
+  public static void d(String tag, String message) {
+    if (logLevel.compareTo(LogLevel.DEBUG) >= 0) {
+      printErrTagMessage(tag, message);
     }
+  }
+
+  public static void e(String tag, String message) {
+    if (logLevel.compareTo(LogLevel.ERROR) >= 0) {
+      printErrTagMessage(tag, message);
+    }
+  }
+
+  public static void i(String tag, String message) {
+    if (logLevel.compareTo(LogLevel.INFO) >= 0) {
+      printTagMessage(tag, message);
+    }
+  }
 }
