2f31e98f113a7fdc259d14ed382eabb0c77d1277
Henrik Sandklef <hesa@sandklef.com>
2019-07-31 00:23:16 +0200

    Unit test constructor

--- src/se/juneday/lifegame/domain/Game.java
+++ src/se/juneday/lifegame/domain/Game.java
@@ -10,104 +10,105 @@ import java.util.Set;
 
 public class Game {
 
-    private String title;
-    private Situation startSitution;
-    private Map<String, Situation> situations;
-    private int situationCount;
-    private int score;
-    private Map<ThingAction, Integer> things;
+  private String title;
+  private Situation startSitution;
+  private Map<String, Situation> situations;
+  private int situationCount;
+  private int score;
+  private Map<ThingAction, Integer> things;
   private Set<String> thingsNeeded;
 
-    private static final String LOG_TAG = Game.class.getSimpleName();  
+  private static final String LOG_TAG = Game.class.getSimpleName();  
   
-    public Game(String title, Map<String, Situation> situations,
-                Situation startSitution, Map<ThingAction, Integer> things,
-                Set<String> thingsNeeded) {
-        this.title = title;
-        this.situations = situations;
-        situations.put("End of game", Situation.endSituation);
-        this.startSitution = startSitution;
-        situationCount = 0;
-        this.things = things;
-        if (things == null) {
-          this.things = new HashMap<>();
-        }
-        this.thingsNeeded = thingsNeeded;
+  public Game(String title, Map<String, Situation> situations,
+              Situation startSitution, Map<ThingAction, Integer> things,
+              Set<String> thingsNeeded) {
+    this.title = title;
+    this.situations = situations;
+    situations.put("End of game", Situation.endSituation);
+    this.startSitution = startSitution;
+    situationCount = 0;
+    this.things = things;
+    if (things == null) {
+      this.things = new HashMap<>();
     }
+    this.thingsNeeded = thingsNeeded;
+  }
 
 
   /* test only - there be dragons here */
-  public Game(int situationCount, int score) {
-        this.title = "Testing game";
-        this.situations = null;
-        this.things = new HashMap<>();
-        this.situations = new HashMap<>();
-        situations.put("End of game", Situation.endSituation);
-        this.situationCount = situationCount;
-        this.score = score;
+  public Game(int situationCount, int score, Map<ThingAction, Integer> things) {
+    this.title = "Testing game";
+    this.situations = null;
+    this.things = new HashMap<>();
+    this.situations = new HashMap<>();
+    situations.put("End of game", Situation.endSituation);
+    this.situationCount = situationCount;
+    this.score = score;
+    this.things = things;
   }
   
-    public Situation getSituation(String title) {
-        return situations.get(title);
-    }
+  public Situation getSituation(String title) {
+    return situations.get(title);
+  }
 
-    public Map<String, Situation> situations() {
-        return situations;
-    }
+  public Map<String, Situation> situations() {
+    return situations;
+  }
 
-    public Situation startSitution() {
-        return startSitution;
-    }
+  public Situation startSitution() {
+    return startSitution;
+  }
 
-    public int situationCount() {
-        return situationCount;
-    }
+  public int situationCount() {
+    return situationCount;
+  }
 
-    public void incSituationCount() {
-        situationCount++;
-    }
+  public void incSituationCount() {
+    situationCount++;
+  }
 
-    public int score() {
-        return score;
-    }
+  public int score() {
+    return score;
+  }
 
-    public int incScore(int amount) {
-        return score += amount;
-    }
+  public int incScore(int amount) {
+    return score += amount;
+  }
 
-    public int decScore(int amount) {
-        return score -= amount;
-    }
+  public int decScore(int amount) {
+    return score -= amount;
+  }
 
-    public Map<ThingAction, Integer> things() {
-        return things;
-    }
+  public Map<ThingAction, Integer> things() {
+    return things;
+  }
 
-    public void addThing(ThingAction thing) {
-        Integer count = things.get(thing);
-        if (count == null) {
-          Log.i(LOG_TAG, "  Things, adding first " + thing);
-          things.put(thing, 1);
-        } else {
-          Log.i(LOG_TAG, "  Things, adding one more " + thing);
-          things.put(thing, count + 1);
-        }
+  public void addThing(ThingAction thing) {
+    Integer count = things.get(thing);
+    if (count == null) {
+      Log.i(LOG_TAG, "  Things, adding first " + thing);
+      things.put(thing, 1);
+    } else {
+      Log.i(LOG_TAG, "  Things, adding one more " + thing);
+      things.put(thing, count + 1);
     }
+  }
 
   /*  public void dropThing(ThingAction ta) {
       dropThing(ta);
-    }
+      }
   */
   
   public void dropThing(ThingAction thing) {
-        Integer count = things.get(thing);
-        if (count != null) {
-            if (count == 1) {
-                things.remove(thing);
-            } else if (count > 0) {
-              things.put(thing, count - 1);
-            }
-        }
+    Integer count = things.get(thing);
+    if (count != null) {
+      if (count == 1) {
+        things.remove(thing);
+      } else if (count > 0) {
+        things.put(thing, count - 1);
+      }
+    }
   }
 
   public Set<String> thingsNeeded() {
@@ -115,15 +116,15 @@ public class Game {
   }
   
 
-    @Override
-    public String toString() {
-        return "Game{" +
-                "title='" + title + '\'' +
-                ", startSitution=" + startSitution +
-                ", situations=" + situations +
-                ", situationCount=" + situationCount +
-                ", score=" + score +
-                ", things=" + things +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Game{" +
+      "title='" + title + '\'' +
+      ", startSitution=" + startSitution +
+      ", situations=" + situations +
+      ", situationCount=" + situationCount +
+      ", score=" + score +
+      ", things=" + things +
+      '}';
+  }
 }
